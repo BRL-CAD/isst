64bd1077a9b24e007e3c24fa5df2fce0df6868e6
erikgreenwald <erikgreenwald@2f96ce8b-6d43-0410-b8df-bffccc660ffb>
2010-04-29 20:45:15 +0000

    stub build stuff for isst_sdl
    git-svn-id: https://brlcad.svn.sourceforge.net/svnroot/brlcad/isst/trunk@38919 2f96ce8b-6d43-0410-b8df-bffccc660ffb

---
+++ sdl/main.c
@@ -0,0 +1,43 @@
+/*
+ * ISST
+ *
+ * Copyright (c) 2005-2010 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file main.c
+ *
+ * top level for SDL version of ISST
+ *
+ */
+
+#include <SDL.h>
+
+int main(int argc, char **argv)
+{
+    SDL_Init (SDL_INIT_VIDEO | SDL_INIT_TIMER);
+    atexit (SDL_Quit);
+    return 0;
+}
+
+/*
+ * Local Variables:
+ * mode: C
+ * tab-width: 8
+ * indent-tabs-mode: t
+ * c-file-style: "stroustrup"
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
