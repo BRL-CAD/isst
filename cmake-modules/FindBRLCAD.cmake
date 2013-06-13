db8efc68cf50523dda0a0610d60df668436d6d3f
Erik <erikgreenwald@users.sourceforge.net>
2013-06-13 14:05:24 -0400

    downcase keywords, add footer, indent

--- cmake-modules/FindBRLCAD.cmake
+++ cmake-modules/FindBRLCAD.cmake
@@ -1,23 +1,23 @@
 #########################################################################
 #
 #	BRL-CAD
-#	
+#
 #	Copyright (c) 1997-2010 United States Government as represented by
 #	the U.S. Army Research Laboratory.
-#	
+#
 #	This library is free software; you can redistribute it and/or
 #	modify it under the terms of the GNU Lesser General Public License
 #	version 2.1 as published by the Free Software Foundation.
-#	
+#
 #	This library is distributed in the hope that it will be useful, but
 #	WITHOUT ANY WARRANTY; without even the implied warranty of
 #	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 #	Lesser General Public License for more details.
-#	
+#
 #	You should have received a copy of the GNU Lesser General Public
 #	License along with this file; see the file named COPYING for more
 #	information.
-#	
+#
 #########################################################################
 #	@file /cmake/FindBRLCAD.cmake
 #
@@ -35,97 +35,97 @@
 #
 #########################################################################
 
-MESSAGE(STATUS "\tSearching for BRLCAD...")
+message(STATUS "\tSearching for BRLCAD...")
 
-IF(RT3_VERBOSE_CMAKE_OUTPUT)   
-    MESSAGE(STATUS "\t\tEnviornment Variable 'PATH': $ENV{PATH}")
-ENDIF(RT3_VERBOSE_CMAKE_OUTPUT)  
+if(RT3_VERBOSE_CMAKE_OUTPUT)
+  message(STATUS "\t\tEnviornment Variable 'PATH': $ENV{PATH}")
+endif(RT3_VERBOSE_CMAKE_OUTPUT)
 
 #First, find the install directories.
-IF(BRLCAD_BASE_DIR)
-    MESSAGE(STATUS "\t\t Using BRLCAD_BASE_DIR...")
-    #if BRLCAD_BASE_DIR is set, then this makes it easy!
-    SET(BRLCAD_BIN_DIR "${BRLCAD_BASE_DIR}/bin")
-    SET(BRLCAD_INC_DIRS "${BRLCAD_BASE_DIR}/include" "${BRLCAD_BASE_DIR}/include/brlcad" "${BRLCAD_BASE_DIR}/include/openNURBS" "${BRLCAD_BASE_DIR}/include/tie")
-    SET(BRLCAD_LIB_DIR "${BRLCAD_BASE_DIR}/lib")
-ELSE(BRLCAD_BASE_DIR)
-    MESSAGE(STATUS "\t\t Searching for BRLCAD components...")
-    #if BRLCAD_BASE_DIR is NOT set, then search for files KNOWN to be in the BRLCAD installation.
+if(BRLCAD_BASE_DIR)
+  message(STATUS "\t\t Using BRLCAD_BASE_DIR...")
+  #if BRLCAD_BASE_DIR is set, then this makes it easy!
+  set(BRLCAD_BIN_DIR "${BRLCAD_BASE_DIR}/bin")
+  set(BRLCAD_INC_DIRS "${BRLCAD_BASE_DIR}/include" "${BRLCAD_BASE_DIR}/include/brlcad" "${BRLCAD_BASE_DIR}/include/openNURBS" "${BRLCAD_BASE_DIR}/include/tie")
+  set(BRLCAD_LIB_DIR "${BRLCAD_BASE_DIR}/lib")
+else(BRLCAD_BASE_DIR)
+  message(STATUS "\t\t Searching for BRLCAD components...")
+  #if BRLCAD_BASE_DIR is NOT set, then search for files KNOWN to be in the BRLCAD installation.
 
-    #Find /bin
-    FIND_PATH(BRLCAD_BIN_DIR brlcad-config "$ENV{PATH}")
-    IF(NOT BRLCAD_BIN_DIR)
- 	    MESSAGE(STATUS "\t\t Could not find BRLCAD bin directory anywhere in paths: $ENV{PATH}")
-	    RETURN()
-    ENDIF(NOT BRLCAD_BIN_DIR)
+  #Find /bin
+  find_path(BRLCAD_BIN_DIR brlcad-config "$ENV{PATH}")
+  if(NOT BRLCAD_BIN_DIR)
+    message(STATUS "\t\t Could not find BRLCAD bin directory anywhere in paths: $ENV{PATH}")
+    return()
+  endif(NOT BRLCAD_BIN_DIR)
 
-    #Find include directories (aka more than one)
-    SET(HEADERS_TO_SEARCH_FOR brlcad/bu.h bu.h opennurbs.h )
-       
-    FOREACH (tHead ${HEADERS_TO_SEARCH_FOR})
-            
-        FIND_PATH(_HEADER_DIR_${tHead} ${tHead} "$ENV{PATH}")
-    
-        IF(RT3_VERBOSE_CMAKE_OUTPUT)   
-            IF(_HEADER_DIR_${tHead})
-                MESSAGE(STATUS "\t\t\t'${tHead}' was found: ${_HEADER_DIR_${tHead}}")
-            ELSE(_HEADER_DIR_${tHead})
-                MESSAGE(STATUS "\t\t\t'${tHead}' was NOT found.")
-            ENDIF(_HEADER_DIR_${tHead})
-        ENDIF(RT3_VERBOSE_CMAKE_OUTPUT)       
-       
-        IF(_HEADER_DIR_${tHead})
-            SET(BRLCAD_INC_DIRS ${BRLCAD_INC_DIRS} ${_HEADER_DIR_${tHead}})
-            SET(BRLCAD_HEADERS_FOUND ${BRLCAD_HEADERS_FOUND} ${tHead})        
-        ELSE(_HEADER_DIR_${tHead})
-            SET(BRLCAD_HEADERS_NOTFOUND ${BRLCAD_HEADERS_NOTFOUND} ${tHead})        
-        ENDIF(_HEADER_DIR_${tHead})
-    
-    ENDFOREACH (tHead ${HEADERS_TO_SEARCH_FOR})
-    
-    IF(NOT BRLCAD_INC_DIRS)
-      	MESSAGE(STATUS "\t\tCould not find BRLCAD include directories anywhere in paths: $ENV{PATH}")
-    	RETURN()
-    ENDIF(NOT BRLCAD_INC_DIRS)
+  #Find include directories (aka more than one)
+  set(HEADERS_TO_SEARCH_FOR brlcad/bu.h bu.h opennurbs.h )
 
-    #Find /lib
-    IF (UNIX)
-        SET(LIB_EXT ".so")
-    ELSE (UNIX)
-        SET(LIB_EXT ".lib")
-    ENDIF(UNIX)
-    
-    FIND_PATH(BRLCAD_LIB_DIR "libbu${LIB_EXT}")
-    
-    IF(NOT BRLCAD_LIB_DIR)
-    	MESSAGE(STATUS "\t\tCould not find brlcad library directory in: $ENV{PATH}")
-    	RETURN()
-    ENDIF(NOT BRLCAD_LIB_DIR)
+  foreach (tHead ${HEADERS_TO_SEARCH_FOR})
 
-ENDIF(BRLCAD_BASE_DIR)
+    find_path(_HEADER_DIR_${tHead} ${tHead} "$ENV{PATH}")
+
+    if(RT3_VERBOSE_CMAKE_OUTPUT)
+      if(_HEADER_DIR_${tHead})
+	message(STATUS "\t\t\t'${tHead}' was found: ${_HEADER_DIR_${tHead}}")
+      else(_HEADER_DIR_${tHead})
+	message(STATUS "\t\t\t'${tHead}' was NOT found.")
+      endif(_HEADER_DIR_${tHead})
+    endif(RT3_VERBOSE_CMAKE_OUTPUT)
+
+    if(_HEADER_DIR_${tHead})
+      set(BRLCAD_INC_DIRS ${BRLCAD_INC_DIRS} ${_HEADER_DIR_${tHead}})
+      set(BRLCAD_HEADERS_FOUND ${BRLCAD_HEADERS_FOUND} ${tHead})
+    else(_HEADER_DIR_${tHead})
+      set(BRLCAD_HEADERS_NOTFOUND ${BRLCAD_HEADERS_NOTFOUND} ${tHead})
+    endif(_HEADER_DIR_${tHead})
+
+  endforeach (tHead ${HEADERS_TO_SEARCH_FOR})
+
+  if(NOT BRLCAD_INC_DIRS)
+    message(STATUS "\t\tCould not find BRLCAD include directories anywhere in paths: $ENV{PATH}")
+    return()
+  endif(NOT BRLCAD_INC_DIRS)
+
+  #Find /lib
+  if (UNIX)
+    set(LIB_EXT ".so")
+  else (UNIX)
+    set(LIB_EXT ".lib")
+  endif(UNIX)
+
+  find_path(BRLCAD_LIB_DIR "libbu${LIB_EXT}")
+
+  if(NOT BRLCAD_LIB_DIR)
+    message(STATUS "\t\tCould not find brlcad library directory in: $ENV{PATH}")
+    return()
+  endif(NOT BRLCAD_LIB_DIR)
+
+endif(BRLCAD_BASE_DIR)
 
 
 
 #Attempt to get brlcad version.
-FIND_PROGRAM(BRLCAD_CONFIGEXE brlcad-config)
-IF(BRLCAD_CONFIGEXE)
-	EXECUTE_PROCESS(COMMAND ${BRLCAD_CONFIGEXE} --version OUTPUT_VARIABLE BRLCAD_VERSION)
-	STRING(STRIP ${BRLCAD_VERSION} BRLCAD_VERSION)
-	
-	IF(BRLCAD_VERSION)
-    	STRING(REGEX REPLACE "([0-9]+)\\.[0-9]+\\.[0-9]+" "\\1" BRLCAD_MAJOR_VERSION "${BRLCAD_VERSION}")
-    	STRING(REGEX REPLACE "[0-9]+\\.([0-9]+)\\.[0-9]+" "\\1" BRLCAD_MINOR_VERSION "${BRLCAD_VERSION}")
-    	STRING(REGEX REPLACE "[0-9]+\\.[0-9]+\\.([0-9]+)" "\\1" BRLCAD_PATCH_VERSION "${BRLCAD_VERSION}")
-        SET(BRLCAD_VERSION_FOUND TRUE)
-    ELSEIF(BRLCAD_VERSION)
-        MESSAGE(STATUS "\t\t'brlcad-config --version' was found and executed, but produced no output.")
-        SET(BRLCAD_VERSION_FOUND FALSE)
-    ENDIF(BRLCAD_VERSION)
+find_program(BRLCAD_CONFIGEXE brlcad-config)
+if(BRLCAD_CONFIGEXE)
+  execute_process(COMMAND ${BRLCAD_CONFIGEXE} --version OUTPUT_VARIABLE BRLCAD_VERSION)
+  string(STRIP ${BRLCAD_VERSION} BRLCAD_VERSION)
 
-ELSE(BRLCAD_CONFIGEXE)
-    MESSAGE(STATUS "\t\tCould not locate 'brlcad-config'.")
-    SET(BRLCAD_VERSION_FOUND FALSE)
-ENDIF(BRLCAD_CONFIGEXE)
+  if(BRLCAD_VERSION)
+    string(REGEX REPLACE "([0-9]+)\\.[0-9]+\\.[0-9]+" "\\1" BRLCAD_MAJOR_VERSION "${BRLCAD_VERSION}")
+    string(REGEX REPLACE "[0-9]+\\.([0-9]+)\\.[0-9]+" "\\1" BRLCAD_MINOR_VERSION "${BRLCAD_VERSION}")
+    string(REGEX REPLACE "[0-9]+\\.[0-9]+\\.([0-9]+)" "\\1" BRLCAD_PATCH_VERSION "${BRLCAD_VERSION}")
+    set(BRLCAD_VERSION_FOUND TRUE)
+  elseif(BRLCAD_VERSION)
+    message(STATUS "\t\t'brlcad-config --version' was found and executed, but produced no output.")
+    set(BRLCAD_VERSION_FOUND FALSE)
+  endif(BRLCAD_VERSION)
+
+else(BRLCAD_CONFIGEXE)
+  message(STATUS "\t\tCould not locate 'brlcad-config'.")
+  set(BRLCAD_VERSION_FOUND FALSE)
+endif(BRLCAD_CONFIGEXE)
 
 #TODO need to make the BRLCAD version checking a requirement for coreInterface, but nothing else.
 #TODO figure out why brlcad-config isn't present on Windows.
@@ -133,79 +133,86 @@ ENDIF(BRLCAD_CONFIGEXE)
 ##########################################################################
 #Search for Libs
 
-SET(LIBS_TO_SEARCH_FOR 
-	analyze
-	bn
-	brlcad
-	bu
-	cursor
-	dm
-	exppp
-	express
-	fb
-	fft
-	gcv
-	ged
-	icv
-	multispectral
-	openNURBS
-	optical
-	orle
-	pkg
-	png14
-	png
-	regex
-	render
-	rt
-	stepcore
-	stepdai
-	stepeditor
-	steputils
-	sysv
-	termio
-	utahrle
-	wdb
-)
+set(LIBS_TO_SEARCH_FOR
+  analyze
+  bn
+  brlcad
+  bu
+  cursor
+  dm
+  exppp
+  express
+  fb
+  fft
+  gcv
+  ged
+  icv
+  multispectral
+  openNURBS
+  optical
+  orle
+  pkg
+  png14
+  png
+  regex
+  render
+  rt
+  stepcore
+  stepdai
+  stepeditor
+  steputils
+  sysv
+  termio
+  utahrle
+  wdb
+  )
 
-MESSAGE(STATUS "BRLCAD_LIB_DIR: ${BRLCAD_LIB_DIR}")
-MESSAGE(STATUS "LIB_EXT: ${LIB_EXT}")
+message(STATUS "BRLCAD_LIB_DIR: ${BRLCAD_LIB_DIR}")
+message(STATUS "LIB_EXT: ${LIB_EXT}")
 
-FOREACH (tlib ${LIBS_TO_SEARCH_FOR})
-    FIND_LIBRARY(_BRLCAD_LIBRARY_${tlib} ${tlib} ${BRLCAD_LIB_DIR} )
+foreach (tlib ${LIBS_TO_SEARCH_FOR})
+  find_library(_BRLCAD_LIBRARY_${tlib} ${tlib} ${BRLCAD_LIB_DIR} )
 
-    IF(RT3_VERBOSE_CMAKE_OUTPUT)   
-        IF(_BRLCAD_LIBRARY_${tlib})
-            MESSAGE(STATUS "\t\t'${tlib}' was found: ${_BRLCAD_LIBRARY_${tlib}}")
-        ELSE(_BRLCAD_LIBRARY_${tlib})
-            MESSAGE(STATUS "\t\t'${tlib}' was NOT found.")
-        ENDIF(_BRLCAD_LIBRARY_${tlib})
-    ENDIF(RT3_VERBOSE_CMAKE_OUTPUT)       
-   
-    IF(_BRLCAD_LIBRARY_${tlib})
-        SET(BRLCAD_LIBRARIES ${BRLCAD_LIBRARIES} ${_BRLCAD_LIBRARY_${tlib}})
-        SET(BRLCAD_LIBRARIES_FOUND ${BRLCAD_LIBRARIES_FOUND} ${tlib})        
-    ELSE(_BRLCAD_LIBRARY_${tlib})
-        SET(BRLCAD_LIBRARIES_NOTFOUND ${BRLCAD_LIBRARIES_NOTFOUND} ${tlib})        
-    ENDIF(_BRLCAD_LIBRARY_${tlib})
+  if(RT3_VERBOSE_CMAKE_OUTPUT)
+    if(_BRLCAD_LIBRARY_${tlib})
+      message(STATUS "\t\t'${tlib}' was found: ${_BRLCAD_LIBRARY_${tlib}}")
+    else(_BRLCAD_LIBRARY_${tlib})
+      message(STATUS "\t\t'${tlib}' was NOT found.")
+    endif(_BRLCAD_LIBRARY_${tlib})
+  endif(RT3_VERBOSE_CMAKE_OUTPUT)
 
-ENDFOREACH (tlib ${LIBS_TO_SEARCH_FOR})
+  if(_BRLCAD_LIBRARY_${tlib})
+    set(BRLCAD_LIBRARIES ${BRLCAD_LIBRARIES} ${_BRLCAD_LIBRARY_${tlib}})
+    set(BRLCAD_LIBRARIES_FOUND ${BRLCAD_LIBRARIES_FOUND} ${tlib})
+  else(_BRLCAD_LIBRARY_${tlib})
+    set(BRLCAD_LIBRARIES_NOTFOUND ${BRLCAD_LIBRARIES_NOTFOUND} ${tlib})
+  endif(_BRLCAD_LIBRARY_${tlib})
+
+endforeach (tlib ${LIBS_TO_SEARCH_FOR})
 
 ##########################################################################
 #Print status
-MESSAGE(STATUS "")
-MESSAGE(STATUS "\t\t Discovered BRLCAD Version ${BRLCAD_VERSION}")
-MESSAGE(STATUS "\t\t BRLCAD_BIN_DIR:     ${BRLCAD_BIN_DIR}")
-MESSAGE(STATUS "\t\t BRLCAD_INC_DIRS:    ${BRLCAD_INC_DIRS}")
-MESSAGE(STATUS "\t\t BRLCAD_LIB_DIR:     ${BRLCAD_LIB_DIR}")
-MESSAGE(STATUS "\t\t BRLCAD_LIBRARIES_FOUND:    ${BRLCAD_LIBRARIES_FOUND}")
-MESSAGE(STATUS "\t\t BRLCAD_LIBRARIES_NOTFOUND: ${BRLCAD_LIBRARIES_NOTFOUND}")
-MESSAGE(STATUS "\t\t BRLCAD_CONFIGEXE: ${BRLCAD_CONFIGEXE}")
-IF(BRLCAD_CONFIGEXE)
-    MESSAGE(STATUS "\t\t\t BRLCAD_MAJOR_VERSION: ${BRLCAD_MAJOR_VERSION}")
-    MESSAGE(STATUS "\t\t\t BRLCAD_MINOR_VERSION: ${BRLCAD_MINOR_VERSION}")
-    MESSAGE(STATUS "\t\t\t BRLCAD_PATCH_VERSION: ${BRLCAD_PATCH_VERSION}")
-ENDIF(BRLCAD_CONFIGEXE)
-MESSAGE(STATUS "")
+message(STATUS "")
+message(STATUS "\t\t Discovered BRLCAD Version ${BRLCAD_VERSION}")
+message(STATUS "\t\t BRLCAD_BIN_DIR:     ${BRLCAD_BIN_DIR}")
+message(STATUS "\t\t BRLCAD_INC_DIRS:    ${BRLCAD_INC_DIRS}")
+message(STATUS "\t\t BRLCAD_LIB_DIR:     ${BRLCAD_LIB_DIR}")
+message(STATUS "\t\t BRLCAD_LIBRARIES_FOUND:    ${BRLCAD_LIBRARIES_FOUND}")
+message(STATUS "\t\t BRLCAD_LIBRARIES_NOTFOUND: ${BRLCAD_LIBRARIES_NOTFOUND}")
+message(STATUS "\t\t BRLCAD_CONFIGEXE: ${BRLCAD_CONFIGEXE}")
+if(BRLCAD_CONFIGEXE)
+  message(STATUS "\t\t\t BRLCAD_MAJOR_VERSION: ${BRLCAD_MAJOR_VERSION}")
+  message(STATUS "\t\t\t BRLCAD_MINOR_VERSION: ${BRLCAD_MINOR_VERSION}")
+  message(STATUS "\t\t\t BRLCAD_PATCH_VERSION: ${BRLCAD_PATCH_VERSION}")
+endif(BRLCAD_CONFIGEXE)
+message(STATUS "")
 
 #Set found flag
-SET(BRLCAD_FOUND TRUE)
+set(BRLCAD_FOUND TRUE)
+
+# Local Variables:
+# tab-width: 8
+# mode: cmake
+# indent-tabs-mode: t
+# End:
+# ex: shiftwidth=2 tabstop=8
